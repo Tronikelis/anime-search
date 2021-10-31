286efb6ba0870e26971a58c86ddb31bfa62f0997
Donatas Bucinskas <donatas@tronikel.com>
2021-10-31 11:08:20 +0200

    bigger font at the start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <div className="text-frost-200 font-bold">Hello, look up some anime!</div>
+            <div className="text-frost-200 font-bold text-6xl text-center">Hello, look up some anime!</div>
         </div>
     );
 }
