23f46ec88bbc832ab7b2477c5fe7dad7d592a987
Donatas Bucinskas <donatas@tronikel.com>
2021-10-31 22:58:49 +0200

    index page font a bit smaller

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div className="w-full h-full flex justify-center items-center p-12">
-            <div className="text-frost-200 font-bold text-8xl text-center">
+            <div className="text-frost-200 font-bold text-3xl md:text-6xl text-center">
                 Hello, look up some anime!
             </div>
         </div>
