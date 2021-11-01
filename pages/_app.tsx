245c2f0bba2b1c6c3ef4ed52e92aea0b8c4f6b96
Donatas Bucinskas <donatas@tronikel.com>
2021-11-01 13:01:11 +0200

    add loading indicators on page change, very nice 👍

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "../styles/index.css";
+import Router from "next/router";
 import type { AppProps } from "next/app";
 import { Layout } from "../components";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import "../styles/index.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
